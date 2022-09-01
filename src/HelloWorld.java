37c9ad17f7f8108fa53ac74c220907a424360a1a
Olav Elnan <s236370@oslomet.no>
2022-09-01 14:34:59 +0200

    first build

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+
+        System.out.println("Teller til ti");
+        for(int i=1; i<11; ++i){
+            System.out.println("Tallet er:");
+            System.out.println(i);
+
+        }
+
+    }
+
+}
